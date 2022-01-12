46c2791c437bc95b1a2cb494d5c86388dd9b3ecc
HTRGOLD <54968893+HTRGOLD@users.noreply.github.com>
2022-01-11 22:39:53 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const { resolveNaptr } = require("dns");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+// app.post("/", function(req,res){
+
+// })
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server is running on port 3000!");
+})
